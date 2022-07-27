69bc1309315ccd8f3614970d3eb2e686bc478595
daren farina <daren.farina@gmail.com>
2022-07-27 16:52:52 -0400

    commit tree b38c2b502fdb6a4a30c42f7062780b435f327f99

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+var multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10);
+
+const num3 = 6;
+const num4 = 22;
+var mod = num4 % num3 
+
+const max = Math.max(5, 10, 15, 20)
+
